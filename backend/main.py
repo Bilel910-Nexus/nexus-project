a63409df8f166f076144c82c600c2210a832cecc
Bilel <bilel910@hotmail.com>
2025-12-03 22:59:55 +0100

    Fix CORS error allow all

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],  # <--- METTRE UNE ÉTOILE ICI (Ça veut dire : Tout le monde rentre)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
